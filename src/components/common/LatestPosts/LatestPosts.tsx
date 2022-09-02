1078ced113583c292598bab7a312ea6512f38939
thevioletmaniac <thevioletmaniac@gmail.com>
2022-09-02 21:18:30 +0600

    Fixed latest posts count

--- src/components/common/LatestPosts/LatestPosts.tsx
+++ src/components/common/LatestPosts/LatestPosts.tsx
@@ -12,20 +12,24 @@ import styles from './latest-posts.module.scss';
 const LatestPosts = (): ReactElement => {
   const bind = classNames.bind(styles);
 
-  const renderLatestPosts = useMemo(() => posts.map((post) => {
-    const { id, title, highlight, link, category, date, tags } = post;
+  const renderLatestPosts = useMemo(() => {
+    const latestPosts = posts.slice(0, 2);
 
-    return (
-      <Preview
-        key={id}
-        title={title}
-        highlight={highlight}
-        link={link}
-        category={category}
-        date={date}
-        tags={tags} />
-    )
-  }), [posts]);
+    return latestPosts?.map((post) => {
+      const { id, title, highlight, link, category, date, tags } = post;
+
+      return (
+        <Preview
+          key={id}
+          title={title}
+          highlight={highlight}
+          link={link}
+          category={category}
+          date={date}
+          tags={tags} />
+      );
+    })
+  }, [posts]);
 
   return (
     <div className='container'>
