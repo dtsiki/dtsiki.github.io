3c2e7b75723020cd0d9c6f7d8130d89fe7aa1d89
thevioletmaniac <thevioletmaniac@gmail.com>
2022-10-02 20:54:40 +0600

    Redesigned loader

--- src/components/layout/Loader/Loader.tsx
+++ src/components/layout/Loader/Loader.tsx
@@ -1,11 +1,21 @@
 import React, { ReactElement } from 'react';
+
+import Window from 'src/components/common/Window';
 import Hero from '../Hero';
+
+import styles from './loader.module.scss';
 
 const Loader = (): ReactElement => {
   return (
     <Hero>
-      <div className='loader'>
-        <span className='visually-hidden'>Loading...</span>
+      <div className={styles.loader}>
+        <Window title='Loading...'>
+          <div className={styles.loader__wrapper}>
+            <div className={styles.loader__bar}>
+              <div className={styles.loader__progress} />
+            </div>
+          </div>
+        </Window>
       </div>
     </Hero>
   );
