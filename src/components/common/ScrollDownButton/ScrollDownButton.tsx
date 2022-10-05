1523b694cc4d90d965b2334c6f6a72f4d3d4da35
thevioletmaniac <thevioletmaniac@gmail.com>
2022-10-05 14:41:50 +0600

    Added scroll down button animation

--- src/components/common/ScrollDownButton/ScrollDownButton.tsx
+++ src/components/common/ScrollDownButton/ScrollDownButton.tsx
@@ -1,5 +1,4 @@
 import React, { MutableRefObject, ReactElement } from 'react';
-
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
@@ -17,9 +16,10 @@ interface ScrollDownButtonProps {
   targetRef:  MutableRefObject<HTMLElement | null>;
   isHidden?: boolean;
   handleScroll?: () => void;
+  isJumping?: boolean;
 }
 
-const ScrollDownButton = ({ targetRef, variant = ScrollDownButtonVariant.PRIMARY, isHidden, handleScroll }: ScrollDownButtonProps): ReactElement => {
+const ScrollDownButton = ({ targetRef, variant = ScrollDownButtonVariant.PRIMARY, isHidden, handleScroll, isJumping = false }: ScrollDownButtonProps): ReactElement => {
   const bind = classNames.bind(styles);
 
   const onScrollDown = (): void => {
@@ -34,7 +34,7 @@ const ScrollDownButton = ({ targetRef, variant = ScrollDownButtonVariant.PRIMARY
     <div className={bind([styles.scrollDownButton, { [styles.scrollDownButton_hidden]: isHidden } ])}>
       <button
         onClick={onScrollDown}
-        className={bind(['button', styles.scrollDownButton__button, styles[variant]])}>
+        className={bind(['button', styles.scrollDownButton__button, styles[variant]], { [styles.jumping]: isJumping })}>
         <span className='visually-hidden'>Scroll down</span>
         <FontAwesomeIcon icon={faAngleDown} />
       </button>
