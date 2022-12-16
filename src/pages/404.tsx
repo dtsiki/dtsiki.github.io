36c4638f570bf82443a683932502c6e14d0f26b9
dtsiki <thevioletmaniac@gmail.com>
2022-12-16 14:39:35 +0600

    Adjusted 404 page template

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import { useEffect } from 'react';
 import Link from 'next/link';
-import { faArrowLeft, faFaceFrown } from '@fortawesome/free-solid-svg-icons';
+import { faFaceFrown } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import Hero from 'src/components/layout/Hero';
 import { HeroVariant } from 'src/components/layout/Hero/Hero';
 import Tag, { TagVariant } from 'src/components/common/Tag/Tag';
+import { useStoreon } from 'storeon/react';
+import { ThemeColor, ThemerEvent } from 'src/store/themer';
 
 import styles from './404.module.scss';
 
 const Custom404 = (): JSX.Element => {
+  const { dispatch } = useStoreon();
+
+  useEffect(() => {
+    dispatch(ThemerEvent.SET_TOP_COLOR, ThemeColor.WHITE);
+  }, []);
+
   return (
     <div className={styles.custom404}>
       <Hero variant={HeroVariant.PRIMARY}>
@@ -25,7 +33,6 @@ const Custom404 = (): JSX.Element => {
               <Tag
                 variant={TagVariant.LIGHT}
                 isOutlined>
-                <FontAwesomeIcon icon={faArrowLeft} />
                 Back to home
               </Tag>
             </a>
