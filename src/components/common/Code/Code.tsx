abb28d93a7a07b7724d3626dbc6e0a1ea5e3d727
dtsiki <thevioletmaniac@gmail.com>
2022-12-16 13:40:15 +0600

    Adjusted code component

--- src/components/common/Code/Code.tsx
+++ src/components/common/Code/Code.tsx
@@ -1,6 +1,6 @@
-import React, { ReactElement } from 'react';
+import { ReactElement } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCopy, faFile } from '@fortawesome/free-solid-svg-icons';
+import { faCopy, faFile, faTerminal } from '@fortawesome/free-solid-svg-icons';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { ascetic } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
 
@@ -28,9 +28,19 @@ interface Props {
   showOnlyCode?: boolean;
   showLineNumbers?: boolean;
   startingLineNumber?: number;
+  isTerminal?: boolean;
 }
 
-const Code = ({ name, customName, code, language, showOnlyCode = false, showLineNumbers = true, startingLineNumber }: Props): ReactElement => {
+const Code = ({
+  name,
+  customName,
+  code,
+  language,
+  showOnlyCode = false,
+  showLineNumbers = true,
+  startingLineNumber,
+  isTerminal = false
+}: Props): ReactElement => {
   const copyToClipboard= (): void => {
     navigator.clipboard.writeText(code);
   };
@@ -39,10 +49,18 @@ const Code = ({ name, customName, code, language, showOnlyCode = false, showLine
     <div className={styles.code}>
       {showOnlyCode ? (
         <div className={styles.code__highlighter}>
+          <div className={styles.code__actions}>
+            <button
+              className={styles.code__control}
+              onClick={copyToClipboard}>
+              <FontAwesomeIcon icon={faCopy} />
+              <span className='visually-hidden'>Copy to clipboard</span>
+            </button>
+          </div>
           <SyntaxHighlighter
             startingLineNumber={startingLineNumber}
-            language={language}
-            showLineNumbers
+            language={language || 'plaintext'}
+            showLineNumbers={showLineNumbers}
             style={ascetic}>
             {code}
           </SyntaxHighlighter>
@@ -51,7 +69,7 @@ const Code = ({ name, customName, code, language, showOnlyCode = false, showLine
         <div className={styles.code__wrapper}>
           <div className={styles.code__header}>
             <div className={styles.code__name}>
-              <FontAwesomeIcon icon={faFile} />
+              <FontAwesomeIcon icon={isTerminal ? faTerminal : faFile} />
               {customName || (language && name) && `${name}.${CodeExtension[language]}`}
             </div>
             <div className={styles.code__actions}>
@@ -65,8 +83,8 @@ const Code = ({ name, customName, code, language, showOnlyCode = false, showLine
           </div>
           <div className={styles.code__body}>
             <SyntaxHighlighter
-              language={language}
-              showLineNumbers={showLineNumbers}
+              language={language || 'plaintext'}
+              showLineNumbers={isTerminal ? false : showLineNumbers}
               style={ascetic}>
               {code}
             </SyntaxHighlighter>
