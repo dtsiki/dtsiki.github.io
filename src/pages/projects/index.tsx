c71c67703190bd785667003f512ed673f8e8fbc6
dtsiki <thevioletmaniac@gmail.com>
2022-12-28 15:39:49 +0600

    Fixed projects page menu color

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,4 +1,4 @@
-import { MutableRefObject, ReactElement, useMemo, useRef } from 'react';
+import { MutableRefObject, ReactElement, useEffect, useMemo, useRef, useState } from 'react';
 import classNames from 'classnames';
 import { faFileText, faFolder } from '@fortawesome/free-solid-svg-icons';
 
@@ -14,10 +14,18 @@ import Planner18Minutes from './_planner-18-minutes';
 import ProjectsCommercial from 'src/components/pages/projects/Commercial';
 import Shortcut from 'src/components/common/Shortcut';
 import { ShortcutVariant } from 'src/components/common/Shortcut/Shortcut';
+import { ThemeColor, ThemerEvent } from 'src/store/themer';
+import { useStoreon } from 'storeon/react';
+import { isElementVisible } from 'src/utils';
+import useEventListener, { Event } from 'src/hooks/useEventListener';
 
 import styles from './projects.module.scss';
 
 const Projects = (): ReactElement => {
+  const [topColor, setTopColor] = useState<ThemeColor>(ThemeColor.WHITE);
+  const [bottomColor, setBottomColor] = useState<ThemeColor>(ThemeColor.WHITE);
+
+  const headerRef = useRef<HTMLDivElement>(null);
   const projectsRef = useRef<HTMLDivElement>(null);
   const quoteGeneratorRef = useRef<HTMLDivElement>(null);
   const todoRef = useRef<HTMLDivElement>(null);
@@ -28,6 +36,44 @@ const Projects = (): ReactElement => {
   const commercialRef = useRef<HTMLDivElement>(null);
 
   const bind = classNames.bind(styles);
+
+  const { dispatch } = useStoreon();
+
+  const handleScroll = (): void => {
+    const bottomOffset = {
+      top: document.documentElement.clientHeight - 50,
+      bottom: document.documentElement.clientHeight
+    };
+
+    if (isElementVisible(headerRef)) {
+      setTopColor(ThemeColor.WHITE);
+    }
+
+    if (isElementVisible(headerRef, bottomOffset)) {
+      setBottomColor(ThemeColor.WHITE);
+    }
+
+    if (isElementVisible(projectsRef)) {
+      setTopColor(ThemeColor.VIOLET);
+    }
+
+    if (isElementVisible(projectsRef, bottomOffset)) {
+      setBottomColor(ThemeColor.VIOLET);
+    }
+  };
+
+  useEventListener(Event.SCROLL, handleScroll);
+
+  useEffect(() => {
+    dispatch(ThemerEvent.SET_TOP_COLOR, topColor);
+    dispatch(ThemerEvent.SET_BOTTOM_COLOR, bottomColor);
+  }, []);
+
+  useEffect(() => {
+    if (topColor) {
+      dispatch(ThemerEvent.SET_TOP_COLOR, topColor);
+    }
+  }, [topColor]);
 
   const shortcuts = [
     {
@@ -97,31 +143,33 @@ const Projects = (): ReactElement => {
   return (
     <div className={styles.projects}>
       <ProgressScroll />
-      <div ref={projectsRef}>
+      <div ref={headerRef}>
         <Hero variant={HeroVariant.PRIMARY}>
           {renderShortcuts}
         </Hero>
       </div>
-      <div ref={todoRef}>
-        <Todo />
-      </div>
-      <div ref={quoteGeneratorRef}>
-        <QuoteGenerator />
-      </div>
-      <div ref={minutesRef}>
-        <Planner18Minutes />
-      </div>
-      <div ref={ticTacToeRef}>
-        <TicTacToe />
-      </div>
-      <div ref={dictionaryRef}>
-        <Dictionary />
-      </div>
-      <div ref={taskManagerRef}>
-        <TaskManager />
-      </div>
-      <div ref={commercialRef}>
-        <ProjectsCommercial />
+      <div ref={projectsRef}>
+        <div ref={todoRef}>
+          <Todo />
+        </div>
+        <div ref={quoteGeneratorRef}>
+          <QuoteGenerator />
+        </div>
+        <div ref={minutesRef}>
+          <Planner18Minutes />
+        </div>
+        <div ref={ticTacToeRef}>
+          <TicTacToe />
+        </div>
+        <div ref={dictionaryRef}>
+          <Dictionary />
+        </div>
+        <div ref={taskManagerRef}>
+          <TaskManager />
+        </div>
+        <div ref={commercialRef}>
+          <ProjectsCommercial />
+        </div>
       </div>
     </div>
   );
