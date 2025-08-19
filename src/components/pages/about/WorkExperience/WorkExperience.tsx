854edf40290a801be033d0c7035e2016cd166502
dtsiki <thevioletmaniac@gmail.com>
2025-08-19 17:49:12 +0600

    Updated my work experience

--- src/components/pages/about/WorkExperience/WorkExperience.tsx
+++ src/components/pages/about/WorkExperience/WorkExperience.tsx
@@ -5,45 +5,44 @@ const AboutWorkExperience = (): ReactElement => {
     {
       company: 'Infomaximum',
       position: 'frontend developer',
-      start: 'apr 2023'
+      start: 'apr 2023',
+      end: 'aug 2025',
     },
     {
       company: 'Ronas IT',
       position: 'frontend developer',
       start: 'aug 2021',
-      end: 'feb 2023'
+      end: 'feb 2023',
     },
     {
       company: 'ADCI Solutions',
       position: 'frontend developer',
       start: 'june 2020',
-      end: 'aug 2021'
+      end: 'aug 2021',
     },
     {
       company: 'Central Design Bureau of Automation',
       position: 'DSP engineer',
       start: 'sep 2014',
-      end: 'jan 2020'
+      end: 'jan 2020',
     },
     {
       company: 'freelance',
       position: 'web developer and designer',
       start: 'sep 2009',
-      end: 'sep 2014'
-    }
+      end: 'sep 2014',
+    },
   ];
 
   return (
     <ul className='list'>
       {workExperiences.map((workExperience) => (
-        <li
-          className='list__item'
-          key={workExperience.company}>
-          <h3 className='list__title'>
-            {workExperience.position}
-          </h3>
+        <li className='list__item' key={workExperience.company}>
+          <h3 className='list__title'>{workExperience.position}</h3>
           <p>{workExperience.company}</p>
-          <p className='list__footer'>{workExperience.start} - {workExperience.end ? workExperience.end : 'currently employed'}</p>
+          <p className='list__footer'>
+            {workExperience.start} - {workExperience.end ? workExperience.end : 'currently employed'}
+          </p>
         </li>
       ))}
     </ul>
